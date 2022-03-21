766059f0ac29d4eb670f54494a9c396df71a5469
nithya-raj <101385689+nithya-raj@users.noreply.github.com>
2022-03-22 01:03:43 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import csv
+file = open("CUSTOMER_SAMPLE_DATA.csv")
+csvreader = csv.reader(file)
+header = next(csvreader)
+print(header)
+rows = []
+for row in csvreader:
+    rows.append(row)
+print(rows)
+file.close()
